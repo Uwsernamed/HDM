fd089f02d63ac66970f07d1ffd321c76c73bc849
Uwsernamed <107601155+Uwsernamed@users.noreply.github.com>
2023-05-31 11:38:05 +0500

    Added some important files.

---
+++ src/main.go
@@ -0,0 +1,219 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+var dictionary string
+var dict string
+var currentDir string
+
+func main() {
+	// Welcome!
+	fmt.Println("HDF Manager\nversion: 1.0.0\n ")
+
+	// Getting the current working directory
+	var err error
+	currentDir, err = os.Getwd()
+	if err != nil {
+		fmt.Println("Failed to retrieve the current working directory:", err)
+		return
+	}
+
+	// Time to read the user's command then send it
+	var command string
+	scanner := bufio.NewScanner(os.Stdin)
+	for 1 == 1 {
+		fmt.Printf("HDM %s | ", currentDir)
+		if !scanner.Scan() {
+			err = scanner.Err()
+			if err != nil {
+				fmt.Println("Failed to read input:", err)
+			} 
+			break
+		}
+		command = scanner.Text()
+		if command == "exit" {
+			break
+		}
+		parsedp := strings.Split(strings.TrimSpace(command), " ")
+		
+		interpreter := &Interpreter{
+			parsed: parsedp,
+		}
+		switch len(interpreter.parsed) {
+		case 1:
+			(interpreter).One()
+		case 2:
+			(interpreter).Two()
+		default:
+			fmt.Println("No function found for the given integer")
+		}
+
+	}
+}
+
+type Interpreter struct {
+    parsed []string
+}
+
+func (r *Interpreter) One() {
+	switch r.parsed[0] {
+	case "help":
+		O_help()
+	case "load":
+		O_load()
+	case "create":
+		O_create()
+	default:
+		fmt.Println("Unknown command. Type 'help' for a list of available commands.\n ")
+	}
+}
+
+func (r *Interpreter) Two() {
+    switch r.parsed[0] {
+	case "select":
+		T_select(r.parsed[1])
+	case "cd":
+
+	default:
+		fmt.Println("Unknown command. Type 'help' for a list of available commands.\n ")
+	}
+}
+
+func O_help() {
+	fmt.Println("Available commands:")
+	fmt.Println("- help: Display a list of available commands.")
+	fmt.Println("- load: ___")
+	fmt.Println("- create: ___")
+	fmt.Println("- exit: Exit the HDF Manager.\n ")
+	fmt.Println("- select [file-location]: selects a HDF file.")
+	fmt.Println("- cd [___]: ___\n ")
+	// Add more commands and descriptions as needed
+}
+
+func O_load() {
+	if dictionary != "" {
+		file, err := os.Open(dictionary)
+		if err != nil {
+			fmt.Println("Error opening file:", err)
+			return
+		}
+		defer file.Close() // Make sure to close the file when done
+
+		// Read the file content
+		content, err := ioutil.ReadAll(file)
+		if err != nil {
+			fmt.Println("Error reading file:", err)
+			return
+		}
+		dict = string(content)
+		// Print the content
+		fmt.Println("Dictionary successfully loaded.\n ")
+	}
+}
+
+func O_create() {
+	// Specify the directory path
+	directory := currentDir // Replace with your desired directory path
+
+	// Get the base folder name
+	baseFolder := filepath.Base(directory)
+
+	// Get all subfolders within the directory
+	subfolders, err := getSubfolders(directory)
+	if err != nil {
+		log.Fatalf("Failed to get subfolders: %s", err)
+	}
+
+	// Format the subfolders as required
+	text := formatSubfolders(subfolders)
+
+	// Create the file
+	filename := fmt.Sprintf("%s/%s.HDF", directory, baseFolder)
+	err = ioutil.WriteFile(filename, []byte(text), 0644)
+	if err != nil {
+		log.Fatalf("Failed to create file: %s", err)
+	}
+
+	fmt.Printf("File '%s' created successfully.\n\n", filename)
+}
+
+func T_select(T_arg string) {
+	if hdfOpen(T_arg) == 1 {
+		dictionary = T_arg
+		fmt.Printf("selected: %s.\n \n", dictionary)
+	} else {
+		fmt.Println("unable to select.\n ")
+	}
+}
+
+func hdfOpen(finderArg string) int {
+
+	_, err := os.Stat(finderArg)
+	if err != nil {
+		if os.IsNotExist(err) {
+
+		} else {
+			fmt.Println("Error:", err)
+		}
+	} else {
+		return 1
+	}
+	return 0
+}
+
+func getSubfolders(directory string) ([]string, error) {
+	var subfolders []string
+
+	err := filepath.Walk(directory, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		if info.IsDir() && path != directory {
+			subfolders = append(subfolders, path)
+		}
+		return nil
+	})
+
+	return subfolders, err
+}
+
+func formatSubfolders(subfolders []string) string {
+	var builder strings.Builder
+	var fsfarr []int
+
+	for _, afolder := range subfolders {
+		fsfarr = append(fsfarr, strings.Count(afolder, string(filepath.Separator)))
+	}
+	fsfleastNumber := findLeastNumber(fsfarr)
+
+	for _, folder := range subfolders {
+		indent := strings.Repeat("    ", strings.Count(folder, string(filepath.Separator))-fsfleastNumber)
+		builder.WriteString(fmt.Sprintf("%s[%s]\n", indent, filepath.Base(folder)))
+	}
+
+	return builder.String()
+}
+
+func findLeastNumber(arr []int) int {
+	if len(arr) == 0 {
+		return -1 // Return -1 if the array is empty
+	}
+
+	minNumber := arr[0] // Assume the first element is the minimum
+
+	for _, num := range arr {
+		if num < minNumber {
+			minNumber = num
+		}
+	}
+
+	return minNumber
+}
